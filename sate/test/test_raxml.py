18540806b74d966c6fe64a14aea686fc482939ff
Jamie Oaks <joaks1@gmail.com>
2012-04-17 02:16:13 -0500

    Added tests for fasttree to test_raxml.py, and made tree estimator tests run in non-developer environment. Had to change tearDown to account for lack of temp file cleanup by FastTree.

--- sate/test/test_raxml.py
+++ sate/test/test_raxml.py
@@ -4,12 +4,14 @@ import unittest
 import datetime
 import logging
 import os
+import shutil, glob
 
 from sate.test import get_testing_configuration, data_source_path, TestLevel, is_test_enabled
 from sate import get_logger, log_exception
 from sate.alignment import Alignment, MultiLocusDataset
 from sate.scheduler import jobq, start_worker
 from sate.filemgr import TempFS
+from sate.usersettingclasses import StringUserSetting, IntUserSetting
 
 _LOG = get_logger(__name__)
 
@@ -20,14 +22,21 @@ class TreeEstimatorTest(unittest.TestCase):
     def setUp(self):
         self.ts = TempFS()
         self.ts.create_top_level_temp(prefix='treeEstimatorTest', parent=os.curdir)
+        self.filename = data_source_path('mafft.anolis.fasta')
+        self.alignment = Alignment()
+        self.alignment.read_filepath(data_source_path('mafft.anolis.fasta'),
+                'FASTA')
 
     def tearDown(self):
-        dir_list = self.ts.get_remaining_directories()
-        for dir in dir_list:
-            try:
-                self.ts.remove_dir(dir)
-            except ValueError:
-                pass
+        dir = self.ts.top_level_temp
+        shutil.rmtree(dir)
+        # dir_list = self.ts.get_remaining_directories()
+        # for dir in dir_list:
+        #     try:
+        #         self.ts.remove_dir(dir)
+        #     except ValueError:
+        #         pass
+
     def get_tree_estimator(self, name):
         try:
             return config.create_tree_estimator(name=name, temp_fs=self.ts)
@@ -40,7 +49,8 @@ your installation is not configured to run this tool.
 """ % name)
             return None
 
-    def _impl_test_tree_estimator(self, name, datatype, partitions):
+    def _impl_test_tree_estimator(self, name, datatype, partitions, **kwargs):
+        num_cpus = kwargs.get('num_cpus', None)
         filename = data_source_path('anolis.fasta')
 
         md = MultiLocusDataset()
@@ -54,18 +64,42 @@ your installation is not configured to run this tool.
             _LOG.warn("test%s skipped" % name)
             return
         # alignment.datatype = datatype
-        a = te.run(alignment=md,
-                   partitions=partitions,
-                   tmp_dir_par=self.ts.top_level_temp,
-                   delete_temps=True)
+        if num_cpus:
+            a = te.run(alignment=md,
+                       partitions=partitions,
+                       tmp_dir_par=self.ts.top_level_temp,
+                       delete_temps=True,
+                       num_cpus=num_cpus)
+        else:
+            a = te.run(alignment=md,
+                       partitions=partitions,
+                       tmp_dir_par=self.ts.top_level_temp,
+                       delete_temps=True)
 
     def testRaxml(self):
-        filename = data_source_path('mafft.anolis.fasta')
-        alignment = Alignment()
-        alignment.read_filepath(filename, 'FASTA')
+        # if is_test_enabled(TestLevel.SLOW, _LOG):
+        self._impl_test_tree_estimator('raxml', datatype="DNA",
+                partitions=[("DNA", 1, 1456)])
 
-        if is_test_enabled(TestLevel.SLOW, _LOG):
-            self._impl_test_tree_estimator('raxml', datatype="DNA", partitions=[("DNA", 1, 1456)])
+    def testFastTree(self):
+        config.fasttree.add_option('options', StringUserSetting(
+                name='options',
+                default='', 
+                short_name=None,
+                help='Options to be passed to FastTree.',
+                subcategory=None))
+        self._impl_test_tree_estimator('fasttree', datatype="DNA",
+                partitions=[("DNA", 1, 1456)])
+
+    def testFastTreeMP(self):
+        config.fasttree.add_option('options', StringUserSetting(
+                name='options',
+                default='',
+                short_name=None,
+                help='Options to be passed to FastTree.',
+                subcategory=None))
+        self._impl_test_tree_estimator('fasttree', datatype="DNA",
+                partitions=[("DNA", 1, 1456)], num_cpus=2)
 
 if __name__ == "__main__":
     unittest.main()

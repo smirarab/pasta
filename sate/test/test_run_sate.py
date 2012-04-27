7afc157a14e80abaf0983fa11de7ca37d1de0f61
Jamie Oaks <joaks1@gmail.com>
2012-04-26 23:44:27 -0500

    added testSingleLocusRun and testMultLocuRun to test_run_sate

--- sate/test/test_run_sate.py
+++ sate/test/test_run_sate.py
@@ -6,9 +6,10 @@ import sys
 import subprocess
 import logging
 
-from sate.test import get_testing_configuration, data_source_path, TestLevel, is_test_enabled
-from sate import get_logger
 import sate
+from sate.test import get_testing_configuration, data_source_path, TestLevel, is_test_enabled, TESTS_DIR
+from sate import get_logger
+from sate.filemgr import TempFS
 
 _LOG = get_logger(__name__)
 config = get_testing_configuration()
@@ -16,9 +17,16 @@ config = get_testing_configuration()
 class RunSateTest(unittest.TestCase):
     def setUp(self):
         self.script_path = os.path.join(sate.sate_home_dir(), 'run_sate.py')
+        self.ts = TempFS()
+        self.ts.create_top_level_temp(prefix='runSateTest',
+                parent=TESTS_DIR)
+        self.anolis_file = data_source_path('anolis.fasta')
+        self.multi_dir = data_source_path('testmulti')
 
     def tearDown(self):
-        pass
+        dir_list = self.ts.get_remaining_directories()
+        for dir in dir_list:
+            self.ts.remove_dir(dir)
 
     def _exe_run_sate(self, args, return_code=0, stdout=None, stderr=None):
         if isinstance(args, str):
@@ -26,6 +34,7 @@ class RunSateTest(unittest.TestCase):
         else:
             arg_list = args
         cmd = ['python', self.script_path] + arg_list
+        _LOG.debug("Command:\n\t" + " ".join(cmd))
         p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE)
         o, e = p.communicate()
@@ -40,6 +49,35 @@ class RunSateTest(unittest.TestCase):
         self._exe_run_sate(['--blah'], return_code=2)
         self._exe_run_sate([], return_code=1)
         self._exe_run_sate(['-h'], return_code=0)
+
+    def testSingleLocusRun(self):
+        if is_test_enabled(TestLevel.EXHAUSTIVE, _LOG,
+                module_name=".".join([self.__class__.__name__,
+                        sys._getframe().f_code.co_name])):
+            arg_list = ['-d', 'dna',
+                        '-j', 'satejob',
+                        '--keepalignmenttemps',
+                        '--keeptemp',
+                        '--temporaries=%s' % self.ts.top_level_temp,
+                        '--iter-limit=1',
+                        '-o', self.ts.top_level_temp,
+                        '-i', self.anolis_file,]
+            self._exe_run_sate(arg_list, return_code=0)
+
+    def testMultLocuRun(self):
+        if is_test_enabled(TestLevel.EXHAUSTIVE, _LOG,
+                module_name=".".join([self.__class__.__name__,
+                        sys._getframe().f_code.co_name])):
+            arg_list = ['-d', 'dna',
+                        '-j', 'satejob',
+                        '--keepalignmenttemps',
+                        '--keeptemp',
+                        '--temporaries=%s' % self.ts.top_level_temp,
+                        '--iter-limit=1',
+                        '-m',
+                        '-o', self.ts.top_level_temp,
+                        '-i', self.multi_dir,]
+            self._exe_run_sate(arg_list, return_code=0)
 
 if __name__ == "__main__":
     unittest.main()

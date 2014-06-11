c6f61ef5a2186740581004ec753aae3404e55fa4
Siavash Mirarab <toranj>
2014-06-11 10:21:59 -0500

    use 4 cpus for FastTree instead of 3

--- pasta/tools.py
+++ pasta/tools.py
@@ -762,7 +762,7 @@ class FastTree(TreeEstimator):
             else:
                 mp_path = self.exe + 'MP'
             if os.path.exists(mp_path):
-                os.environ["OMP_NUM_THREADS"] = str(num_cpus if num_cpus < 4 else 3)
+                os.environ["OMP_NUM_THREADS"] = str(num_cpus if num_cpus < 5 else 4)
                 invoc[0] = mp_path
 
         dirs_to_delete = []

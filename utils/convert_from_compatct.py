5b3f2ff25a9a6f95dd72e65d761d4ffc4c85e249
Siavash Mirarab <toranj>
2014-06-12 12:26:49 -0500

    move utils to a new dir

--- utils/convert_from_compatct.py
+++ utils/convert_from_compatct.py
@@ -6,11 +6,11 @@ Created on Aug 8, 2013
 import sys
 from pasta.alignment import Alignment
 
-def compact_to_fasta(inf):
+def compact_to_fasta(inf, out):
     a = Alignment()
     a.read_filepath(inf,"COMPACT3")
-    a.write('%s.FASTA' %inf, 'FASTA')
+    a.write(out, 'FASTA')
 
 if __name__ == '__main__':
-    compact_to_fasta(sys.argv[1])
+    compact_to_fasta(sys.argv[1], '%s.FASTA' %sys.argv[1] if len(sys.argv) == 1 else sys.argv[2])
     

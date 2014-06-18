7fc6f608ced075178def7f7f97c3ad9fd19b1b35
Siavash Mirarab <toranj>
2014-06-17 19:52:59 -0500

    add icon

--- make-app.sh
+++ make-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -r build dist
-python setup.py py2app --resources run_pasta.py,bin,run_seqtools.py 2>&1 |tee py2app.log; 
+python setup.py py2app --resources run_pasta.py,bin,run_seqtools.py --iconfile pasta.ico 2>&1 |tee py2app.log; 
 chmod +x dist/PASTA.app/Contents/Resources/bin/*;

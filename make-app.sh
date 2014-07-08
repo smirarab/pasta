4a6b364b5a3729945d4faa6b33bf2c43f320c3ab
Siavash Mirarab <toranj>
2014-07-08 11:59:51 -0500

    small edits to make-app script

--- make-app.sh
+++ make-app.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-rm -r build dist
+set -u
+set -e
+set -x
+set -o pipefail
+
+rm -fr build dist
+
 python setup.py py2app --resources run_pasta.py,bin,run_seqtools.py --iconfile pasta.ico 2>&1 |tee py2app.log; 
+
 chmod +x dist/PASTA.app/Contents/Resources/bin/*;
